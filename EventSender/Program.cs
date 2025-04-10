f3efa98b625adb2694f4f9d7cadbf0577fea7bcd
Igor Tishkin <igor@tapelectric.app>
2025-04-10 10:34:26 +0200

    init

---
+++ EventSender/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+var client = DeviceClient.CreateFromConnectionString(args[0]);
+
+var i = 0;
+
+while (true)
+{
+    var obj = new
+    {
+        event_id = Guid.NewGuid().ToString("N"),
+        device_id = "azaza-1",
+        event_produced_utc = DateTime.UtcNow,
+        form = "liquid",
+        temperature = 20.05D + (i),
+        temperature_unit = "C",
+        humidity = 0.5D,
+        humidity_unit = "RH",
+        pressure = 6000.00D + i * 100,
+        pressure_unit = "kPa"
+    };
+
+    var json = JsonSerializer.Serialize(obj);
+    var msg = new Message(Encoding.UTF8.GetBytes(json));
+
+    await client.SendEventAsync(msg);
+
+    Console.WriteLine(json);
+
+    await Task.Delay(10000);
+
+    i++;
+}
